174382aca3d64bd2b6347c5be8c2788adc6a27da
Jean-cédric Thérond <jean.cedric.t@gmail.com>
2022-02-11 04:05:26 +0100

    Autocomplete bug fixes

--- frontend-server/next.config.js
+++ frontend-server/next.config.js
@@ -1,21 +1,8 @@
-const withTM = require('next-transpile-modules')([
-  '@mui/material',
-  '@mui/system',
-  '@mui/icons-material', // If @mui/icons-material is being used
-]);
-
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = withTM({
+const nextConfig = {
   poweredByHeader: false,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@mui/styled-engine': '@mui/styled-engine-sc',
-      };
-      return config;
-    }
-});
+};
 
 module.exports = nextConfig;
--- frontend-server/pages/index.tsx
+++ frontend-server/pages/index.tsx
@@ -2,15 +2,7 @@ import React, { useState } from "react";
 import _, { throttle, sortBy } from "lodash";
 import axios from 'axios'
 
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import CircularProgress from '@mui/material/CircularProgress';
@@ -19,34 +11,6 @@ function parseString<Type>(v: Type): string { return v + ''; }
 
 const getRandomInt = (max, min = 0) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
-
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
 
 const StyledTextField = styled(TextField)(({ theme }) => ({
   color: 'inherit',
@@ -65,11 +29,9 @@ const StyledTextField = styled(TextField)(({ theme }) => ({
   },
 }));
 
-
 function communeToOptions(commune) {
   return `${parseString(commune["Code_postal"]).padStart(5, '0')} - ${commune["Nom_commune"]} ${commune["Ligne_5"] ? '| ' + commune["Ligne_5"] : ''}`
 }
-
 
 function Home() {
   const [loading, setLoading] = useState(false);
@@ -80,68 +42,77 @@ function Home() {
 
   dataLoadingPromise.then(setOptions)
 
-  const onChange = (_, value) => {
-    setSelectedLocation(value);
-    setLoading(false);
-  };
+  const onChange = (_, value) => setSelectedLocation(value)
 
-  const handleSearch = throttle((event, value, reason) => {
-    if(reason == "blur") return;
-    if(reason == "clear") {
-      setSearch('');
-      setDataLoadingPromise(Promise.resolve([]))
-      setLoading(false);
-      return;
+  const handleSearch = (_e, value, reason) => {
+    switch (reason) {
+
+      case "reset":
+        setLoading(false);
+      case "blur":
+        return;
+
+      case "clear":
+        setSearch('');
+        setDataLoadingPromise(Promise.resolve(null))
+        return;
+
+      case "input":
+        setLoading(true);
+        if (value === "") {
+          setSearch(value)
+          setLoading(false)
+          setOptions([])
+          return;
+        }
+        throttle(() => {
+          setSearch(value);
+          setOptions([]);
+          setDataLoadingPromise(axios.get(parseInt(value) !== NaN && parseInt(value) > 0 ?
+            `/api/v0/geoloc/byPostalCode/${parseInt(value)}` :
+            `/api/v0/geoloc/byName/${value}`).then((res) => res.data))
+        }, 1000, { leading: false })();
+        return;
+
+
+      default:
+        break;
     }
-    setLoading(true);
-    setSearch(value);
-    setDataLoadingPromise(axios.get(parseInt(value) !== NaN && parseInt(value) > 0 ?
-      `/api/v0/geoloc/byPostalCode/${parseInt(value)}` :
-      `/api/v0/geoloc/byName/${value}`).then((res) => res.data))
-  }, 1500, { leading: false });
+  }
+
 
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <Autocomplete
-                fullWidth
-                style={{
-                  position: "relative"
-                }}
-                id="location-search"
-                options={sortBy(options, "departement")}
-                autoComplete
-                clearOnEscape
-                includeInputInList
-                forcePopupIcon={false}
-                isOptionEqualToValue={(o, v) => communeToOptions(o) === communeToOptions(v)}
-                onInputChange={handleSearch}
-                onChange={onChange}
-                groupBy={v => v.departement}
-                getOptionLabel={v => communeToOptions(v)}
-                renderInput={(params) => (
-                  <StyledTextField {...params}
-                    InputProps={{
-                      ...params.InputProps,
-                      endAdornment: (
-                        <>
-                          {loading && <CircularProgress color="inherit" size={20} />}
-                          {params.InputProps.endAdornment}
-                        </>
-                      ),
-                    }} />
-                )}
-              />
-            </Search>
-          </Toolbar>
-        </AppBar>
-      </Box>
+      <Autocomplete
+        fullWidth
+        style={{
+          position: "relative"
+        }}
+        id="location-search"
+        options={sortBy(options, "departement")}
+        autoComplete
+        clearOnEscape
+        includeInputInList
+        forcePopupIcon={false}
+        isOptionEqualToValue={(o, v) => loading || (communeToOptions(o) === communeToOptions(v))}
+        onInputChange={handleSearch}
+        onChange={onChange}
+        groupBy={v => v.departement}
+        getOptionLabel={v => communeToOptions(v)}
+        renderInput={(params) => (
+          <StyledTextField {...params}
+            label="Desired location"
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                  {loading && <CircularProgress color="inherit" size={20} />}
+                  {params.InputProps.endAdornment}
+                </>
+              ),
+            }} />
+        )}
+      />
 
     </>
   );
